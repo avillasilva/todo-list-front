5fdaef2cbbaad9e502e45ae89d593cc9001ad829
Victor Koehler <koehlervictor@cc.ci.ufpb.br>
2021-07-13 22:59:35 -0300

    Authentication and Fetch mecanism

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,10 +18,10 @@ function LoginForm({Login, error}) {
                     <label htmlFor="name">Name:</label>
                     <input type="text" name="name" id="name" onChange={ e => setDetails({...details, name: e.target.value})} value={details.name}/>
                 </div>
-                <div className="form-group">
+                {/* <div className="form-group">
                     <label htmlFor="Email">Email:</label>
                     <input type="email" name="email" id="email" onChange={ e => setDetails({...details, email: e.target.value})} value={details.email}/>
-                </div>
+                </div> */}
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
                     <input type="password" name="password" id="password" onChange={ e => setDetails({...details, password: e.target.value})} value={details.password}/>
