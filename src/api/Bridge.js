de99c72b2c8daeb7fa36fabb9f7aff9bb59ebbd7
Victor Koehler <koehlervictor@cc.ci.ufpb.br>
2021-07-15 23:42:30 -0300

    Autenticação baseada em Tokens

--- src/api/Bridge.js
+++ src/api/Bridge.js
@@ -5,39 +5,45 @@ function connectToAPI(username, password, api) {
     username = encodeURIComponent(username);
     password = encodeURIComponent(password);
 
+    const COOKIES_INCLUDE = true;
+    const TOKEN_INCLUDE = true;
+
     let t = {};
     t.username = '';
     t._tasklists = undefined;
     t._tasks = undefined;
     t._notes = undefined;
     t._categories = undefined;
+    t._default_headers = { "Content-Type": "application/x-www-form-urlencoded" };
 
     t.getTaskLists = () => t._tasklists;
     t.getTasks = () => t._tasks;
     t.getNotes =  () => t._notes;
     t.getCategories = () => t._categories;
 
-    t.makeRequest = (url, body) => {
+    t._makeRequest = (url, body) => {
         body = body || {};
         body.method = body.method || "GET";
         body.credentials = body.credentials || "include";
-        body.headers = body.headers || { "Content-Type": "application/x-www-form-urlencoded" };
-        return fetch(api+url, body).then((response) => response.json());
+        body.headers = {...t._default_headers, ...(body.headers || { })};
+        return fetch(api+url, body);
     };
+    t.makeRequest = (url, body) => t._makeRequest(url, body).then((response) => response.json());
 
-    return fetch(api+"todorest/login", {
-        method: "POST",
-        credentials: "include",
-        body: "username="+username+"&password="+password,
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
+    let LOGIN_BODY = { method: 'POST', body: "username="+username+"&password="+password };
+    if (COOKIES_INCLUDE) { LOGIN_BODY.credentials = "include"; }
+    return t._makeRequest("notes/login", LOGIN_BODY).then((response) => response.text()).then((tokens) => {
+        if (TOKEN_INCLUDE) {
+            tokens.trim().split('\n').forEach((i) => {
+                const j = i.trim().split(':');
+                t._default_headers[j[0]] = j[1];
+            });
         }
-    }).then((response) => {
         t.username = username;
-        t.makeRequest('todorest/tasklist').then((i) => t._tasklists = i);
-        t.makeRequest('todorest/task').then((i) => t._tasks = i);
-        t.makeRequest('notes/note').then((i) => t._notes = i);
-        t.makeRequest('todorest/category').then((i) => t._categories = i);
+        t.makeRequest('todorest/tasklist/').then((i) => t._tasklists = i);
+        t.makeRequest('todorest/task/').then((i) => t._tasks = i);
+        t.makeRequest('notes/note/').then((i) => t._notes = i);
+        t.makeRequest('todorest/category/').then((i) => t._categories = i);
         return t;
     });
 }
